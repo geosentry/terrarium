cdc63cc7f4bc381d28c71d22335307be1c6b631e
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-06 18:39:26 +0530

    Add py packaging config files

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name="terrarium",
+    version="0.0.1",
+
+    description='Earth Engine & GIS tooling Python Package for the GeoSentry Platform.',
+    long_description=long_description, 
+    long_description_content_type='text/markdown', 
+
+    url='https://github.com/geosentry/terrarium',
+    project_urls={ 
+        'Bug Reports': 'https://github.com/geosentry/terrarium/issues',
+        'Source': 'https://github.com/geosentry/terrarium',
+    },
+
+    author='Manish Meganathan',
+    author_email='meganathan.manish@gmail.com',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    keywords='geosentry, terrarium, earthengine, GIS',
+
+    packages=find_packages(),
+    python_requires='>=3.8, <4',
+    install_requires=[
+        'earthengine-api==0.1.271',
+        'google-api-core==1.30.0',
+        'google-cloud-firestore==2.1.3',
+        'google-cloud-secret-manager==2.5.0',
+    ],   
+)
