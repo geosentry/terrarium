29cee48d0d9ca0f6ce19aee1523f45ac9663b88a
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-12 22:55:02 +0530

    Restructure temporal module
    - Renamed the conversion functions to be more descriptive
    - Added a generic funciton shift_date instead of next_acquisition
    - Removed the datebuffer generator and combined functionality with the daterange generator
    - Added error handling for functions
    - Renamed the earth engine collection datelist generator

--- terrarium/temporal.py
+++ terrarium/temporal.py
@@ -8,58 +8,67 @@ import typing
 import datetime
 from google.api_core.datetime_helpers import DatetimeWithNanoseconds
 
-def generate_date_timestamp(posixstamp: int) -> datetime.datetime:
+""" A set of conversion functions between different temporal data structures """
+
+def datetime_fromtimestamp(posixstamp: int) -> datetime.datetime:
     """ A function that returns a datetime object for a given POSIX timestamp. """
     return datetime.datetime.utcfromtimestamp(int(posixstamp/1000))
 
-def generate_date_googledate(date: DatetimeWithNanoseconds) -> datetime.datetime:
+def datetime_fromgoogledate(date: DatetimeWithNanoseconds) -> datetime.datetime:
     """ A function that returns a datetime object for a given DatetimeWithNanoseconds object. """
     return datetime.datetime.fromisoformat(date.isoformat()).replace(tzinfo=None)
 
-def generate_googledate_date(date: datetime.datetime) -> DatetimeWithNanoseconds:
+def googledate_fromdatetime(date: datetime.datetime) -> DatetimeWithNanoseconds:
     """ A function that returns a DatetimeWithNanoseconds object for a given datetime object. """
     return DatetimeWithNanoseconds.fromisoformat(date.isoformat())
 
-def generate_date_nextacquisition(date: datetime.datetime) -> datetime.datetime:
+def googledate_fromtimestamp(posixstamp: int) -> DatetimeWithNanoseconds:
+    """ A function that returns a DatetimeWithNanoseconds object for a given POSIX timestamp. """
+    return DatetimeWithNanoseconds.utcfromtimestamp(int(posixstamp/1000))
+
+
+""" A set of manipulation functions for temporal entities """
+
+def shift_date(date: datetime.datetime, shift: int) -> datetime.datetime:
     """ A function that returns a datetime object for the next acquisition given another acquisition date. """
-    # Sentinel-2 MSI Acquisitions occur every 5 days
-    return date + datetime.timedelta(days=5)
+    return date + datetime.timedelta(days=shift)
 
-def generate_daterange(date: datetime.datetime, days: int) -> typing.Tuple[datetime.datetime]:
+def generate_daterange(date: datetime.datetime, width: int, buffer: bool = False) -> typing.Tuple[datetime.datetime]:
     """ 
-    A function that returns a datetime tuple with two dates for a given date and the number 
-    of days in the range. The first date in the tuple represents the beginning of the range and 
-    the second date represents the given date or the end of the range.
+    A function that returns a datetime tuple with two dates that represent the beginning and end of a temporal daterange. 
+    The start of the daterange is 'width' number of days before the given 'date'. The end of the daterange is given 'date'
+    unless the 'buffer' argument is set to True, in which case the end is 'width' number of days ahead of the given 'date'.
     """
-    # Generate a datetime object for the width number of day before the given date
-    begin = date - datetime.timedelta(days=days)
-    # Return the tuple of dates
-    return (begin, date)
+    try:
+        # Generate the start of the daterange
+        start = shift_date(date, -width)
+        # Generate the end of the daterange
+        end = shift_date(date, width) if buffer else date
 
-def generate_datebuffer(date: datetime.datetime, buffer: int) -> typing.Tuple[datetime.datetime]:
-    """
-    A function that returns a datetime tuple with two dates for a given date and the number 
-    of buffer days around it. The first date in the tuple represents the beginning of the range
-    which is the buffer number of days before the given date and the second date represents the
-    end of the range with buffer number of days after the given date.
-    """
-    # Generate a datetime object for the buffer number of days before the given date
-    start = date - datetime.timedelta(days=buffer)
-    # Generate a datetime object for the buffer number of days after the given date
-    end = date + datetime.timedelta(days=buffer)
-    # Return the tuple of dates
-    return (start, end)
+        # Return the tuple of dates
+        return (start, end)
 
-def generate_datelist(collection: ee.ImageCollection) -> typing.List[datetime.datetime]:
+    except Exception as e:
+        raise RuntimeError(f"could not generate daterange. error: {e}")
+
+def generate_earthenginecollection_datelist(collection: ee.ImageCollection) -> typing.List[datetime.datetime]:
     """
     A function that returns a list of datetime object representing the unique dates in a 
-    given Earth Engine ImageCollection. The returned datelist sorted chronologically.
+    given Earth Engine ImageCollection. The returned datelist is sorted chronologically.
     """
-    # Aggregate the timestamps of every Image in the ImageCollection as a list of POSIX timestamps
-    timestamps = collection.aggregate_array("system:time_start").getInfo()
-    # Accumulate the timestamps into a list of datetime objects
-    datelist = [generate_date_timestamp(posix_stamp) for posix_stamp in timestamps]
-    # Sort the datetime objects in the list
-    datelist.sort()
+    if not isinstance(collection, ee.ImageCollection):
+        raise RuntimeError("could not generate datelist. collection must be a ee.ImageCollection.")
+
+    try:
+        # Aggregate the timestamps of every Image in the ImageCollection as a list of POSIX timestamps
+        timestamps = collection.aggregate_array("system:time_start").getInfo()
+        # Accumulate the timestamps into a list of datetime objects
+        datelist = [datetime_fromtimestamp(posix_stamp) for posix_stamp in timestamps]
+        # Sort the datetime objects in the list
+        datelist.sort()
+        
+    except Exception as e:
+        raise RuntimeError(f"could not generate daterange. error: {e}")
+
     # Return the datelist
     return datelist
