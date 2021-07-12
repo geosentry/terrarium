f1710dd92c0e41e055cff2222993af564b560aca
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-12 21:31:47 +0530

    Update earth engine initialization method
    - Updated the initialize method to accept a credentials byte string
    - The credentials string is used to authenticate with Earth engine
    - Removed the dependency on Secret Manager
    - The function is now idempotent and does not initialize a new session when one is already initialized.

--- terrarium/__init__.py
+++ terrarium/__init__.py
@@ -1,42 +1,29 @@
 """
 Terrarium Package
 """
-import ee
-import google.cloud.secretmanager as secretmanager
 
-# from . import pallete
-# from . import temporal
-# from . import spatial
-# from . import spectral
-# from . import acquisition
-# from . import export
-
-def initialize(project: str):
+def initialize(credentials: bytes):
     """
-    A function that initializes an Earth Engine session with a Service Account Agent.
-    The credentials of the Service Account are stored on GCP Secret Manager.
+    A function that initializes an Earth Engine session with the credentials 
+    of a Service Account Agent. The Service Account must be authenticated to use 
+    the Earth Engine API and the credentials must be provided as bytes string.
     """
-    try:
-        # Create a client for the Secret Manager Service.
-        secrets = secretmanager.SecretManagerServiceClient()
-    
-    except Exception as e:
-        raise RuntimeError(f"unable to initialize a secret manager client - {e}")
+    import ee
+
+    # Check if Earth Engine is already intialized
+    if ee.data._initialized:
+        return
+
+    # Check if the credentials are of the correct type
+    if not isinstance(credentials, bytes):
+        raise RuntimeError("could not initialize earth engine. credentials must be a byte string.")
 
     try:
-        # Construct the name of the earth engine secret
-        secret_name = f"projects/{project}/secrets/earthengineone/versions/latest"
-        # Access the secret data using the secret name
-        secret_data = secrets.access_secret_version(name=secret_name)
-
-    except Exception as e:
-        raise RuntimeError(f"unable to access earth engine secret - {e}")
-
-    try:
-        # Construct the Service Account Credentials from the secret data
-        eecredentials = ee.ServiceAccountCredentials(email=None, key_data=secret_data.payload.data)
-        # Use the Service Account Credentials to authenticate the Earth Engine Session
+        # Construct the Service Account Credentials from the credentials bytes string
+        eecredentials = ee.ServiceAccountCredentials(email=None, key_data=credentials)
+        # Use the Service Account Credentials to authenticate 
+        # with Earth Engine and initialize the session
         ee.Initialize(credentials=eecredentials)
 
     except Exception as e:
-        raise RuntimeError(f"unable to initialize earth engine session - {e}")
+        raise RuntimeError(f"could not initialize earth engine. error: {e}")
