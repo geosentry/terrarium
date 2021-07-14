895abcd31898068044d03c2fc8c43f4a3bf38e98
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-14 17:58:39 +0530

    Refactor spatial
    - Added new generation algorithms for the TCI and NDVI indices that replaces the full generation function
    - Added a new function that generates a new spectral image for a given date, geometry and index ID.
    - This refactor will enable the functionality of the geocore-spectral functionality

--- terrarium/spectral.py
+++ terrarium/spectral.py
@@ -5,34 +5,35 @@ The spectral module contains functions for generating
 various spectral manipulation on acquisition images.
 """
 import ee
+import datetime
+
 from . import palette
+from . import temporal
 
-def generate_TrueColor(image: ee.Image) -> ee.Image:
+def truecolor_algorithm(image: ee.Image) -> ee.Image:
     """ 
-    A function that generates a visualized True Color Image for the given Earth Engine Image.
-    The function assumes that the Image is from the Sentinel-2 MSI collection.
+    An algorithm that takes in an ee.Image and outputs an ee.Image. 
+    Suitable for use with ee.ImageCollection.map(algorithm).
 
-    The process of generating a True Color Image in this function includes 
-    three steps - composition, upsampling and visualization.
+    The algorithm transforms the Image into an upsampled True Color Image
+    and assumes the provided Image is from the Sentinel-2 MSI L2A Collection.
+    
+    The transformation process includes two steps - composition and upsampling.
 
     #### Composition
-    True Color images are composed by isolating the true color RGB 
-    bands which are the TCI_R, TCI_G and TCI_B bands.
+    The images are composed by isolating the true color RGB
+    bands which are the 'TCI_R', 'TCI_G' and 'TCI_B' bands.
 
     #### Upsampling
-    True Color images are upsampled with bicubic interpolation at a 
+    The images are upsampled using bicubic interpolation at a 
     1m spatial resolution and then reprojected to the EPSG:4326 CRS.
-
-    ### Visualization
-    True Color images are visualized using a pre-defined 
-    color pallete from the geocore.palette library.
     """
     try:
         # Isolate the True Color band components of the Image
         tcbase = image.select('TCI_R', 'TCI_G', 'TCI_B')
 
     except ee.EEException as e:
-        raise RuntimeError(f"true color composition failed. {e}")
+        raise ee.EEException(f"true color composition failed. {e}")
 
     try:
         # Upsample the true color image with bicubic interpolation
@@ -41,46 +42,37 @@ def generate_TrueColor(image: ee.Image) -> ee.Image:
         upsampled = upsampled.reproject(crs="EPSG:4326", scale=1)
 
     except ee.EEException as e:
-        raise RuntimeError(f"true color upsampling failed. {e}")
-
-    try:
-        # Apply the visualization pallete on the image
-        visualized = upsampled.visualize(**palette.S2TC)
-    
-    except ee.EEException as e:
-        raise RuntimeError(f"true color visualization failed. {e}")
+        raise ee.EEException(f"true color upsampling failed. {e}")
 
     # Return the final true color image
-    return visualized
+    return upsampled
 
+def ndvi_algorithm(image: ee.Image) -> ee.Image:
+    """
+    An algorithm that takes in an ee.Image and outputs an ee.Image. 
+    Suitable for use with ee.ImageCollection.map(algorithm).
 
-def generate_NDVI(image: ee.Image) -> ee.Image:
-    """ 
-    A function that generates a visualized NDVI Image for the given Earth Engine Image. 
-    The function assumes that the Image is from the Sentinel-2 MSI collection.
+    The algorithm transforms the Image into a focalized NDVI Image
+    and assumes the provided Image is from the Sentinel-2 MSI L2A Collection.
 
-    NDVI - Normalized Difference Vegetation Index.
-    It is spectral index used for determing vegetation health. The process 
-    of generating an NDVI Image in this function includes four steps -
-    composition, upsampling, focalization and visualization
+    NDVI - Normalized Difference Vegetation Index is spectral 
+    index used for determing vegetation health.
+
+    The transformation process includes three steps - composition, upsampling and focalization.
 
     #### Composition
-    NDVI images are composed by performing spectral bandmath on the image with 
+    The images are composed by performing spectral bandmath on the image with 
     the equation (NIR-RED)/(NIR+RED) where NIR is the Near-Infrared Wavelength 
     (Band 8) and RED is the Red Wavelength (Band 4).
 
     #### Upsampling
-    NDVI images are upsampled with bicubic interpolation at a 1m spatial 
+    The images are upsampled with bicubic interpolation at a 1m spatial 
     resolution and then reprojected to the EPSG:4326 CRS.
 
     ### Focalization
-    NDVI images are focalized by scaling each pixel value by one order of magnitude
+    The images are focalized by scaling each pixel value by one order of magnitude
     and rounding them down to the nearest whole number. The resultant image is then
     passed through a morphological reducer that uses a square kernel.
-
-    ### Visualization
-    NDVI images are visualized using a pre-defined color pallete 
-    from the geocore.palette library
     """
     try:
         # Isolate the NIR and RED band components and add them to a dictionary
@@ -89,7 +81,7 @@ def generate_NDVI(image: ee.Image) -> ee.Image:
         ndvibase = image.expression("(NIR-RED)/(NIR+RED)", values).rename('NDVI')
         
     except ee.EEException as e:
-        raise RuntimeError(f"ndvi composition failed. {e}")
+        raise ee.EEException(f"ndvi composition failed. {e}")
 
     try:
         # Upsample the NDVI image with bicubic interpolation
@@ -98,7 +90,7 @@ def generate_NDVI(image: ee.Image) -> ee.Image:
         upsampled = upsampled.reproject(crs="EPSG:4326", scale=1)
     
     except ee.EEException as e:
-        raise RuntimeError(f"ndvi upsampling failed. {e}")
+        raise ee.EEException(f"ndvi upsampling failed. {e}")
 
     try:
         # Scale each pixel value in the image by one order of magnitude
@@ -107,14 +99,74 @@ def generate_NDVI(image: ee.Image) -> ee.Image:
         ndvi = ndvi.focal_median(kernelType="square", radius=5)
 
     except ee.EEException as e:
-        raise RuntimeError(f"ndvi focalization failed. {e}")
-
-    try:
-        # Apply the visualization pallete on the image
-        visualized = ndvi.visualize(**palette.NDVIFOCAL)
-
-    except ee.EEException as e:
-        raise RuntimeError(f"ndvi visualization failed. {e}")
+        raise ee.EEException(f"ndvi focalization failed. {e}")
 
     # Return the final NDVI image
-    return visualized
+    return ndvi
+
+def generate_spectral_image(date: datetime.datetime, geometry: ee.Geometry, index: str) -> ee.Image:
+    """ 
+    A function that generates a spectral Image given the date as a datetime object, geometry
+    as an ee.Geometry and a valid spectral index as a string to generate.
+
+    Images are generated from the Sentinel-2 MSI L2A Collection by filtering the collection
+    with a buffer around the given date and mapping the spectral algorithm on each Image in 
+    the filtered ImageCollection. The transformed collection is then mosaic-ed into a single Image, 
+    visualized with corresponding palette and clipped to the given geometry before being returned.
+
+    Valid values for the 'index' argument are:
+    - 'TCI' - True Color Index
+    - 'NDVI' - Normalized Difference Vegetation Index
+    
+    (other values will be supported in the future)
+
+    Refer to the spectral generation algorithm for each Index for details on how they are generated.
+    """
+    # Assign null values for algo and vis
+    algo = None
+    vis = None
+
+    # Check the value of the index and set 
+    # the algorithm and palett accordingly
+    if index == "NDVI":               
+        algo = ndvi_algorithm
+        vis = palette.NDVIFOCAL
+
+    elif index == "TCI":
+        algo = truecolor_algorithm
+        vis = palette.S2TRUECOLOR
+
+    else:
+        # Raise an exception if the index is not valid
+        raise RuntimeError(f"unsupported index: {index}")    
+
+    try:   
+        # Create a temporal buffer around the given date
+        buffer = daterange = temporal.generate_daterange(date, 0.5, buffer=True)
+
+        # Define the Sentinel-2 MSI L2A Collection
+        s2collection = ee.ImageCollection("COPERNICUS/S2_SR")
+        # Filter the collection for the geometry and the temporal buffer
+        collection = s2collection.filterBounds(geometry).filterDate(*buffer)
+    
+    except RuntimeError as e:
+        raise RuntimeError(f"could not create temporal buffer. {e}")
+    except ee.EEException as e:
+        raise RuntimeError(f"could not create filtered collection. {e}")
+
+    try:
+        # Transform the collection by mapping the algorithm over it
+        transformed_collection = collection.map(algo)
+        # Mosaic the transformed collection into a single image.
+        mosiacimage = transformed_collection.mosaic()
+
+        # Apply a visualisation palette to the image.
+        visimage = mosiacimage.visualize(**vis) 
+        # Clip the image to the geometry.
+        image = visimage.clip(geometry)
+
+    except ee.EEException as e:
+        raise RuntimeError(f"could not create spectral image. {e}")
+
+    # Return the image
+    return image
