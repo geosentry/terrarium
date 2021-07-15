363e33f434fc8286a7690d28c1af218da0b5224b
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-15 21:41:58 +0530

    Refactor export module
    - Renamed the image export helper
    - Added error handling for export helper and added the bucket to export to as an argument.
    - CRS of the export is retrieved from the Image.
    - Added a function get_taskstatus to check the status of a task given the project and task ID.

--- terrarium/export.py
+++ terrarium/export.py
@@ -5,7 +5,7 @@ The export module contains functions for exporting acquisition assets
 """
 import ee
 
-def export_image_asset(image: ee.Image, geometry: ee.Geometry, name: str) -> ee.batch.Task:
+def export_image(image: ee.Image, bucket: str, name: str) -> ee.batch.Task:
     """ 
     A function that creates an export task for the given Earth Engine Image.
     The image is exported to the 'terrascope-assets' bucket as a GeoTIFF with the given 
@@ -13,17 +13,51 @@ def export_image_asset(image: ee.Image, geometry: ee.Geometry, name: str) -> ee.
     """
     # Define the export configuration
     exportconfig = {
-        "bucket": "terrascope-assets",
-        "scale": 1,
-        "crs": "EPSG:4326",
-        "maxPixels": 100000000,
-        "fileFormat": "GeoTIFF",
-        "skipEmptyTiles": True,
+        # Export Image
         "image": image,
-        "description": f"export"[0:100],
+
+        # Export Tranforms and Bounds
+        "scale": 1,
+        "region": image.geometry(),
+        "crs": image.projection().crs(),
+        
+        # Destination Filename and Bucket
+        "bucket": bucket,
         "fileNamePrefix": name,
-        "region": geometry
+
+        # Export Description
+        "description": f"export"[0:100],
+        
+        # Export Constraints
+        "maxPixels": 1e10,
+        "skipEmptyTiles": True,
+        "fileFormat": "GeoTIFF"
     }
 
-    # Create and returns the export configuration
-    return ee.batch.Export.image.toCloudStorage(**exportconfig)
+    try:
+        # Create an export task with the export configuration
+        task = ee.batch.Export.image.toCloudStorage(**exportconfig)
+        # Return the task
+        return task
+
+    except Exception as e:
+        raise RuntimeError(f"could not create image export task. {e}.")
+
+def get_taskstatus(taskid: str, project: str) -> dict:
+    """ 
+    A function that returns the status of an Earth Engine export task 
+    given the project ID and the task ID of the export task. 
+
+    The task ID for a Task object can be retrieved from its 'id' property.
+    """
+    try:
+        # Construct the operations identifier from the project ID and task ID
+        opid = f"projects/{project}/operations/{taskid}"
+        # Retrieve the task status from Earth Engine
+        status = ee.data.getOperation(opid)
+
+    except Exception as e:
+        raise RuntimeError(f"could not create image export task. {e}.")
+
+    # Return the task status
+    return status
